d7d286e3035b6c9165998f525dfe67bb7c899bd5
xingyun86 <xingyun_ppshuai@yeah.net>
2018-11-13 00:44:15 +0800

    提交win32和mfc虚表示例

---
+++ src/SkinSB/Win32Demo/KListCtrl.h
@@ -0,0 +1,43 @@
+/*
+** KListCtrl.h
+** KListCtrl Class, Written By Win32 API
+** When You Use This Class, You Must pragma commctl32.lib First
+*/
+
+#ifndef __KLISTCTRL_H__
+#define __KLISTCTRL_H__
+
+#include <Windows.h>
+#include <tchar.h>
+
+typedef struct _lstele LSTELE;
+typedef struct _lstele* PLSTELE;
+
+struct _lstele {
+	int _item;
+	TCHAR _path[MAX_PATH];
+	PLSTELE next;
+};
+
+class KListCtrl
+{
+public:
+	KListCtrl();
+	BOOL BindList(HWND hWnd);
+	BOOL CreateList(HINSTANCE, HWND, POINT, SIZE);
+	~KListCtrl();
+
+	void CreateColumn(UINT _mask, int _cx, TCHAR* _text, int _col);
+	void SetListItem(int _iItem, int _iSubItem, TCHAR* _text);
+	void SetListItemText(int _iItem, int _iSubItem, TCHAR* _text);
+	void AddListPath(LSTELE _ele);
+	PLSTELE GetListPath();
+	BOOL RemoveListItem();
+private:
+	HWND    m_hList;
+	POINT   m_pt;
+	SIZE    m_sz;
+	PLSTELE p_head;
+};
+
+#endif
